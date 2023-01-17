a043437000059557d07e49992ecb91956ff024bf
eyup.kirci <eyupkirci@gmail.com>
2023-01-17 03:09:44 +0100

    Dev: popup.js developed.

---
+++ popup.js
@@ -0,0 +1,29 @@
+// constants
+const color = document.getElementById("input");
+const p1 = document.getElementById("colorCode");
+const p2 = document.getElementById("result");
+const CopyButton = document.getElementById("copyColorClipboard");
+
+// select color & change background
+color.addEventListener("input", (e) => {
+  console.log(e.target.value);
+
+  // document.body.style.backgroundColor = e.target.value;
+  document.getElementById("colorCode").style.backgroundColor = e.target.value;
+
+  p1.innerHTML = e.target.value;
+  p2.innerHTML = "Color Code Captured!..";
+});
+
+// copy color code to clipboard
+CopyButton.addEventListener("click", () => {
+  navigator.clipboard.writeText(
+    document.getElementById("colorCode").textContent
+  );
+  //copy button messages
+  if (p1.textContent.startsWith("#")) {
+    p2.innerHTML = "Copied! Now, You can copy HEX color code...";
+  } else {
+    p2.innerHTML = "Error: Please pick a color!...";
+  }
+});
